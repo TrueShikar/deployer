7f9db6ebca15c79d120ad077006271ea91e3453c
TrueShikar <115390854+TrueShikar@users.noreply.github.com>
2023-02-22 13:00:48 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-rm mirr* && git clone https://github.com/TrueShikar/mirror-leech-telegram-bot && cd mirr* && wget https://raw.githubusercontent.com/TrueShikar/wzml1/heroku/config.env && bash start.sh && python3 -m http.server
+git clone https://github.com/TrueShikar/mirror-leech-telegram-bot && cd mirr* && wget https://raw.githubusercontent.com/TrueShikar/wzml1/heroku/config.env && bash start.sh && python3 -m http.server
